fff3101ba9db5d2853a217aac8d4a20a23e6fe2b
Dustin Aaron Santos <santosdustinaaron07@gmail.com>
2024-08-15 09:40:25 +0800

    add backend and frontend initial files

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
