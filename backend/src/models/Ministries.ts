7756332cfbf4001476ba7075af66541aefbc4007
Dustin Aaron Santos <santosdustinaaron07@gmail.com>
2025-02-12 19:13:42 +0800

    removal of middlewares in ministries

--- backend/src/models/Ministries.ts
+++ backend/src/models/Ministries.ts
@@ -39,63 +39,6 @@ const ministrySchema = new Schema<IMinistry>(
   { timestamps: true }
 );
 
-// [MIDDLEWARE]
-ministrySchema.pre("findOneAndDelete", async function (next) {
-  const ministryId = this.getQuery()["_id"];
-
-  try {
-    await Membership.updateMany(
-      { ministries: ministryId },
-      { $pull: { ministries: ministryId } }
-    );
-
-    next();
-  } catch (err) {
-    next(err as Error);
-  }
-});
-
-ministrySchema.post("save", async function (doc) {
-  const ministryId = doc?._id;
-  await Log.create({
-    action: "created",
-    collection: "Ministry",
-    documentId: doc._id,
-    data: doc.toObject(),
-    performedBy: doc._id,
-    timestamp: new Date(),
-  });
-});
-
-ministrySchema.post("findOneAndUpdate", async function (doc) {
-  if (doc) {
-    // Fetch previous data before update
-    const prevData = doc.toObject();
-
-    await Log.create({
-      action: "updated",
-      collection: "Ministry",
-      documentId: doc._id,
-      data: { prevData, newData: this.getUpdate() },
-      performedBy: this.getQuery()._id,
-      timestamp: new Date(),
-    });
-  }
-});
-
-ministrySchema.post("findOneAndDelete", async function (doc) {
-  if (doc) {
-    await Log.create({
-      action: "deleted",
-      collection: "Ministry",
-      documentId: doc._id,
-      data: doc.toObject(),
-      performedBy: this.getQuery()._id,
-      timestamp: new Date(),
-    });
-  }
-});
-
 // [EXPORT]
 const Ministry = model<IMinistry>("Ministry", ministrySchema);
 export default Ministry;
